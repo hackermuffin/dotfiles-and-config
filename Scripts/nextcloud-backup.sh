0de98d433472fa7fef80741047ef3d555d74713b
Peter Oslington <peteroslington1@gmail.com>
2021-09-20 19:28:38 +1000

    Added script for backing up important folders to Nextcloud WIP

---
+++ Scripts/nextcloud-backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+rclone_filtered="rclone copy --exclude */.git/* --exclude */dosdevices/../* --dump filters"
+#$rclone_filtered
+
+home_folders_to_backup=(\
+	"Zotero" \
+	"Pictures" \
+	"Games" \
+	".local/share/dolphin-emu" \
+	".local/share/DustAET" \
+	".local/share/Terraria" \
+	".local/share/wesnoth" \
+	
+	
+)
+
+remote_path=nextcloud:HomeBackup
+
+for folder in ${home_folders_to_backup[@]}
+do
+  echo "Backing up ~/$home_folders_to_backup to $remote_path/$folder ..."
+  $rclone_filtered --dry-run ~/$folder $remote_path/$folder
+  echo "Backup of ~/$home_folders_to_backup to $remote_path/$folder complete"
+  echo
+done
+
+
+#rclone copy ~/NextCloud/Peter   nextcloud:Peter
+
+
+#rclone copy ~/.local/share/wesnoth nextcloud:HomeBackup/.local/share/wesnoth
+#rclone copy ~/.local/share/Terraria nextcloud:HomeBackup/.local/share/Terraria
+#rclone copy ~/.local/share/dolphin-emu nextcloud:HomeBackup/.local/share/dolphin-emu
+
