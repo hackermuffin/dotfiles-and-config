ba7d3ded1b54aa1696ac76a5b40797e7e8063706
Peter Oslington <peteroslington1@gmail.com>
2021-09-21 19:25:05 +1000

    Added more lines to backup script

--- Scripts/nextcloud-backup.sh
+++ Scripts/nextcloud-backup.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
-rclone_filtered="rclone copy --exclude */.git/* --exclude */dosdevices/../* --dump filters"
-#$rclone_filtered
 
+# Set up filters on rclone command
+rclone_filtered='rclone copy --exclude */.git/*'
+
+# Backup my directory
+$rclone_filtered --dry-run ~/NextCloud/Peter   nextcloud:Peter
+
+# Other home folders to backup
 home_folders_to_backup=(\
 	"Zotero" \
 	"Pictures" \
-	"Games" \
 	".local/share/dolphin-emu" \
 	".local/share/DustAET" \
 	".local/share/Terraria" \
 	".local/share/wesnoth" \
-	
-	
 )
 
+# RClone remote path to store backups
 remote_path=nextcloud:HomeBackup
 
+# Backup all folders
 for folder in ${home_folders_to_backup[@]}
 do
-  echo "Backing up ~/$home_folders_to_backup to $remote_path/$folder ..."
+  echo "Backing up ~/$folder to $remote_path/$folder ..."
   $rclone_filtered --dry-run ~/$folder $remote_path/$folder
-  echo "Backup of ~/$home_folders_to_backup to $remote_path/$folder complete"
+  echo "Backup of ~/$folder to $remote_path/$folder complete"
   echo
 done
 
 
-#rclone copy ~/NextCloud/Peter   nextcloud:Peter
 
 
